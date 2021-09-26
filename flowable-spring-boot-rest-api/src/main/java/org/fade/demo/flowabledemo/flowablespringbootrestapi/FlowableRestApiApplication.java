f9b6d0c8b621824d639f5f383b7561a4ddad81a8
paigeman <53284808+paigeman@users.noreply.github.com>
2021-09-26 09:53:23 +0800

    #commit BPMN开始部分 flowable rest api 使用（通过Spring Boot方式模拟，未完成）

---
+++ flowable-spring-boot-rest-api/src/main/java/org/fade/demo/flowabledemo/flowablespringbootrestapi/FlowableRestApiApplication.java
@@ -0,0 +1,17 @@
+package org.fade.demo.flowabledemo.flowablespringbootrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author fade
+ * @date 2021/09/24
+ */
+@SpringBootApplication
+public class FlowableRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlowableRestApiApplication.class, args);
+    }
+
+}
