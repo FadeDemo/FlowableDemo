1ed887fcaba8c522257245b2624c751d1515a068
paigeman <53284808+paigeman@users.noreply.github.com>
2021-09-30 17:49:06 +0800

    #commit 集成Spring ProcessEngineFactoryBean部分

---
+++ spring-integration/src/main/java/org/fade/demo/flowabledemo/springintegration/Main.java
@@ -0,0 +1,13 @@
+package org.fade.demo.flowabledemo.springintegration;
+
+/**
+ * @author fade
+ * @date 2021/09/30
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+}
