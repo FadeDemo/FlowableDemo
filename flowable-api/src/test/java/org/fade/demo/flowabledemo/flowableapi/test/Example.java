5c518fad255bfc6a3223bf4442a01e01ac067319
paigeman <53284808+paigeman@users.noreply.github.com>
2021-09-30 11:53:46 +0800

    #commit flowable-api 表达式函数和单元测试部分

---
+++ flowable-api/src/test/java/org/fade/demo/flowabledemo/flowableapi/test/Example.java
@@ -0,0 +1,43 @@
+package org.fade.demo.flowabledemo.flowableapi.test;
+
+import org.flowable.engine.ProcessEngine;
+import org.flowable.engine.RuntimeService;
+import org.flowable.engine.TaskService;
+import org.flowable.engine.test.ConfigurationResource;
+import org.flowable.engine.test.Deployment;
+import org.flowable.engine.test.DeploymentId;
+import org.flowable.engine.test.FlowableTest;
+import org.flowable.task.api.Task;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@FlowableTest
+@ConfigurationResource("flowable.custom.cfg.xml")
+public class Example {
+
+    private static final Logger logger = LoggerFactory.getLogger(Example.class);
+
+    private ProcessEngine processEngine;
+    private RuntimeService runtimeService;
+    private TaskService taskService;
+
+    @BeforeEach
+    void setUp(ProcessEngine processEngine) {
+        this.processEngine = processEngine;
+        this.runtimeService = processEngine.getRuntimeService();
+        this.taskService = processEngine.getTaskService();
+    }
+
+    @Test
+    @Deployment(resources = {"holiday-request.bpmn20.xml"})
+//    void testSimpleProcess() {
+    void testSimpleProcess(@DeploymentId String deploymentId) {
+        logger.info("Deployment id is " + deploymentId);
+        runtimeService.startProcessInstanceByKey("holidayRequest");
+        Task task = taskService.createTaskQuery().singleResult();
+        logger.info("Task's name is " + task.getName());
+    }
+
+}
