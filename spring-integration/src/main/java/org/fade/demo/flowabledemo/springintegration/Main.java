17c3708d5502183e8e10066b5695314e85ede3be
paigeman <poizijiang1120@foxmail.com>
2021-10-05 18:32:28 +0800

    #commit 集成Spring 事务部分

--- spring-integration/src/main/java/org/fade/demo/flowabledemo/springintegration/Main.java
+++ spring-integration/src/main/java/org/fade/demo/flowabledemo/springintegration/Main.java
@@ -1,5 +1,7 @@
 package org.fade.demo.flowabledemo.springintegration;
 
+import org.flowable.engine.RepositoryService;
+import org.flowable.engine.repository.Deployment;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -11,6 +13,10 @@ public class Main {
 
     public static void main(String[] args) {
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("flowable.cfg.xml");
+        RepositoryService repositoryService = applicationContext.getBean("repositoryService", RepositoryService.class);
+        Deployment deployment = repositoryService.createDeployment()
+                .addClasspathResource("holiday-request.bpmn20.xml")
+                .deploy();
     }
 
 }
--- spring-integration/src/test/java/org/fade/demo/flowabledemo/springintegration/test/TransactionTest.java
+++ spring-integration/src/test/java/org/fade/demo/flowabledemo/springintegration/test/TransactionTest.java
@@ -1,12 +1,32 @@
 package org.fade.demo.flowabledemo.springintegration.test;
 
-import org.flowable.engine.test.FlowableTest;
+import org.flowable.engine.RepositoryService;
+import org.flowable.engine.test.Deployment;
+import org.flowable.spring.impl.test.SpringFlowableTestCase;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 
-@FlowableTest
 @ContextConfiguration("classpath:flowable.cfg.xml")
-public class TransactionTest {
+public class TransactionTest extends SpringFlowableTestCase {
 
+    private static final Logger logger = LoggerFactory.getLogger(TransactionTest.class);
 
+    @Autowired
+    private RepositoryService repositoryService;
+
+    @Test
+    @Deployment(resources = {"holiday-request.bpmn20.xml"})
+    public void test() {
+        // fixme debug调试有正常语句输出，直接run或者debug返回后无法查找调试记录
+        // fixme 已尝试@FlowableTest 和 extends SpringFlowableTestCase 皆无法实现预期效果，后者甚至会报错
+        org.flowable.engine.repository.Deployment deployment = repositoryService.createDeployment()
+                .addClasspathResource("holiday-request.bpmn20.xml")
+                .deploy();
+        String deploymentId = deployment.getId();
+        logger.info("Deployment id is " + deploymentId);
+    }
 
 }
