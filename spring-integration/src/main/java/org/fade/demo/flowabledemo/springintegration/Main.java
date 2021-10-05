87329420ecbcea5fc0d323c1f00e95375874531d
paigeman <poizijiang1120@foxmail.com>
2021-10-05 12:38:33 +0800

    #commit 集成Spring 事务部分

--- spring-integration/src/main/java/org/fade/demo/flowabledemo/springintegration/Main.java
+++ spring-integration/src/main/java/org/fade/demo/flowabledemo/springintegration/Main.java
@@ -1,5 +1,8 @@
 package org.fade.demo.flowabledemo.springintegration;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * @author fade
  * @date 2021/09/30
@@ -7,7 +10,7 @@ package org.fade.demo.flowabledemo.springintegration;
 public class Main {
 
     public static void main(String[] args) {
-
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("flowable.cfg.xml");
     }
 
 }
---
+++ spring-integration/src/test/java/org/fade/demo/flowabledemo/springintegration/test/TransactionTest.java
@@ -0,0 +1,12 @@
+package org.fade.demo.flowabledemo.springintegration.test;
+
+import org.flowable.engine.test.FlowableTest;
+import org.springframework.test.context.ContextConfiguration;
+
+@FlowableTest
+@ContextConfiguration("classpath:flowable.cfg.xml")
+public class TransactionTest {
+
+
+
+}
