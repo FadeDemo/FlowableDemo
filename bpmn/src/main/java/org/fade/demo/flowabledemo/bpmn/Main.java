379e5db5483de108c035f0196ac5201ef8ac7bf5
paigeman <53284808+paigeman@users.noreply.github.com>
2021-10-11 11:59:42 +0800

    #commit BPMN 介绍部分

--- bpmn/src/main/java/org/fade/demo/flowabledemo/bpmn/Main.java
+++ bpmn/src/main/java/org/fade/demo/flowabledemo/bpmn/Main.java
@@ -2,19 +2,23 @@ package org.fade.demo.flowabledemo.bpmn;
 
 import cn.hutool.core.lang.Assert;
 import cn.hutool.setting.dialect.Props;
-import org.flowable.engine.ProcessEngine;
-import org.flowable.engine.ProcessEngineConfiguration;
-import org.flowable.engine.RepositoryService;
-import org.flowable.engine.RuntimeService;
+import org.flowable.engine.*;
 import org.flowable.engine.impl.cfg.StandaloneProcessEngineConfiguration;
 import org.flowable.engine.repository.Deployment;
 import org.flowable.engine.runtime.ProcessInstance;
+import org.flowable.task.api.Task;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
 
 /**
  * @author fade
  * @date 2021/10/09
  */
 public class Main {
+
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) {
         Props props = new Props("db.properties");
@@ -39,6 +43,15 @@ public class Main {
         // 启动流程实例
         RuntimeService runtimeService = processEngine.getRuntimeService();
         ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("financialReport");
+        // 获取任务列表
+        TaskService taskService = processEngine.getTaskService();
+        List<Task> tasks = taskService.createTaskQuery().taskCandidateGroup("accountancy").list();
+        logger.info("Accountancy group has " + tasks.size() + " task(s)");
+        // 申领任务
+        taskService.claim(tasks.get(0).getId(), "kermit");
+        // 查询任务执行人为kermit的任务列表
+        List<Task> kermitTasks = taskService.createTaskQuery().taskAssignee("kermit").list();
+        logger.info("Kermit has " + kermitTasks.size() + " task(s)");
     }
 
 }
