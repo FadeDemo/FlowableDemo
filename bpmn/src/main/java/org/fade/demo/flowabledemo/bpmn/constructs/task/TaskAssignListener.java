bbf659e2686f247504f7a6d5855279e5c4457db8
paigeman <poizijiang1120@foxmail.com>
2021-10-24 12:07:10 +0800

    #commit BPMN 结构部分

---
+++ bpmn/src/main/java/org/fade/demo/flowabledemo/bpmn/constructs/task/TaskAssignListener.java
@@ -0,0 +1,21 @@
+package org.fade.demo.flowabledemo.bpmn.constructs.task;
+
+import org.flowable.engine.delegate.TaskListener;
+import org.flowable.task.service.delegate.DelegateTask;
+
+/**
+ * @author fade
+ * @date 2021/10/24
+ */
+public class TaskAssignListener implements TaskListener {
+
+    @Override
+    public void notify(DelegateTask delegateTask) {
+        // 直接分配给一个用户
+//        delegateTask.setAssignee("kermit");
+        // 作为候选任务分配
+        delegateTask.addCandidateUser("kermit");
+        delegateTask.addCandidateGroup("management");
+    }
+
+}
